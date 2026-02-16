df1a854df8c30ca6461c7f58e31349554aecbaaf
Ari <arianna.e.penrod-1@ou.edu>
2026-02-16 14:43:54 -0600

    trying to test CI/CD

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
       }}
     >
       <Text>This will be the landing page of our app! Yippee</Text>
+      <Text>Will this work idkkkk</Text>
       <View style={{ height: 20 }} />
       <Text>
         This can be the page that shows the user a list of items near them?
