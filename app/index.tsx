e0685eb7f52ceb3f7a75027bc879fd48d59e121f
Ari <arianna.e.penrod-1@ou.edu>
2026-02-18 14:14:42 -0600

    add number type to getNearbyStore func

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 const GOOGLE_API_KEY = "AIzaSyCnjvFaIkRg6peTExxw3ARtnD61LRFcMP4";
 
-async function getNearbyStores(lat, lng) {
+async function getNearbyStores(lat: number, lng: number) {
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json
   ?location=${lat},${lng}
   &radius=2000
@@ -27,7 +27,7 @@ export default function Index() {
       <Text>Will this work idkkkk</Text>
       <View style={{ height: 20 }} />
       <Text>
-        This can be the page that shows the user a list of items near them? 
+        This can be the page that shows the user a list of items near them?
       </Text>
     </View>
   );
