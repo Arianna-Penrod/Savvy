147b9100de185dd3254d194ff979999b38368745
Sadita06 <Sadita.F.Sadat-1@ou.edu>
2026-03-07 16:13:52 -0600

    changed price of bread

--- data/demoProducts.ts
+++ data/demoProducts.ts
@@ -17,7 +17,7 @@ export const demoProducts = [
     name: "Bread",
     prices: [
       { store: "Demo Walmart", price: 2.49 },
-      { store: "Demo Target", price: 2.99 }
+      { store: "Demo Target", price: 2.29 }
     ]
   }
 ];
