4ea4c852469665e8e1e02513a9f05192da06119c
madipeezy <madscience0809@gmail.com>
2026-03-07 12:58:33 -0600

    test folder

---
+++ app/map.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This will be the landing page of our app! Yippee</Text>
+      <Text>Will this work idkkkk</Text>
+      <View style={{ height: 20 }} />
+      <Text>
+        This can be the page that shows the user a list of items near them?
+      </Text>
+    </View>
+  );
+}
