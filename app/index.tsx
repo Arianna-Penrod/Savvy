dc868b4f9e04631c5d76eb7bc4f0023ee27fe1ff
madipeezy <madscience0809@gmail.com>
2026-03-07 13:28:43 -0600

    test

--- app/index.tsx
+++ app/index.tsx
@@ -34,7 +34,7 @@ export default function Index() {
   const handleLogin = () => {
     // Temporary fake login logic
     // Replace this later with Firebase, Supabase, your backend, etc.
-    if (email.trim() === "test@test.com" && password === "1234") {
+    if (email.trim() === "test@test.com" && password === "12345") {
       setLoginError("");
       setIsLoggedIn(true);
     } else {
