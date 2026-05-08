d06150cb550a642d0ef7ad868820e12c09c332ba
Sarah 3 <Sarah.Haffener05@gmail.com>
2026-05-08 11:19:59 -0500

    Update Email_verification.tsx

--- components/Email_verification.tsx
+++ components/Email_verification.tsx
@@ -1,10 +1,9 @@
 
-// user email is test@test.com ?
-// user password is 123456 ?
-// this first one is to test the set email if the eamil changes
-// then this code will need to change
-
-// read in email
+{/* user email is test@test.com ?
+user password is 123456 
+this first one is to test the set email if the eamil changes
+then this code will need to change
+read in email*/}
 
 type emailVerificationProps = {
     email: string;
@@ -12,14 +11,14 @@ type emailVerificationProps = {
 
 export default function EmailVerification({ email }: emailVerificationProps) {
     if (email === "test@test.com") {
-        // confirm email
+        {/* confirm email*/}
     } else {
-        // reject any other inputs other than test@test.com
+        {/*reject any other inputs other than test@test.com */}
         console.log("Error: Invalid email or password");
     }
 }
 
-//read in password
+{/*read in password*/}
 
 type passwordVerificationProps = {
     password: string;
@@ -27,9 +26,9 @@ type passwordVerificationProps = {
 
 export function PasswordVerification({ password }: passwordVerificationProps) {
     if (password === "123456") {
-        // confirm password
+        {/*confirm password*/}
     } else {
-        // reject any other inputs other than 123456
+        {/*reject any other inputs other than 123456*/}
         console.log("Error: Invalid email or password");
     }
 }
